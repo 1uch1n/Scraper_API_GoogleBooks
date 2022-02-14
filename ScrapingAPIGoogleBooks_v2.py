7e5aaa8e8b8a9e9a2d776e4e4ff9fea6cc5effeb
1uch1n <59725998+1uch1n@users.noreply.github.com>
2022-02-14 14:14:49 -0300

    Update ScrapingAPIGoogleBooks_v2.py

--- ScrapingAPIGoogleBooks_v2.py
+++ ScrapingAPIGoogleBooks_v2.py
@@ -1,6 +1,6 @@
-# Author: Louis Leclerc
+# Author: 1uch1n (1uch1n@protonmail.com)
 # Creation date: 06/10/2020
-# Last updated: 18/11/2020
+# Last update: 14/02/2022
 # Description:
     # scraping API Google Books
     # collect bibliographic data from automated researches
@@ -15,87 +15,118 @@
     # more info on https://developers.google.com/books/docs/overview
 
 
-#importing modules
+#import modules
 import requests #to scrap
 import json     #to read API's response
 import csv      #to read input CSV and write CSV as output
 import unidecode #to turn latin accentuated characters into URL compatible letters
-from time import time #to  analyze time taken by request
+from time import time #to analyze time taken by request
+from time import sleep #to pause scraping process
 
 # location of files
 import os
-os.chdir("/home/luchin/Documents/Projets Data/Scraping BDD theatre")
+os.chdir(input('Set the location of the input files:\n->'))
 
-#definition of upstream variables
-my_file = "input_table.csv"
+# definition of upstream variables
+my_file = input('What is the name of the input file?:\n->')
 urlAPI = "https://www.googleapis.com/books/v1/volumes?q="
 keyAPI = "&key=AIzaSyCbxRGAL4pcMVeL0GoaE9cKVP5LmoimpCU"
-country_query = "&country=CL" #set to research in Chile
 
 
-#function converting each row of a single-column CSV into a string and returns them in a list
 def open_file(file):
-    with open(file, encoding="latin-1") as f:
-        res = f.read().splitlines()
+    '''convert each row of a single-column CSV into a string and returns them as a list'''
+    res = []
+    with open(file, encoding="utf-8") as f:
+        rdr = csv.reader(f)
+        for row in rdr:
+            print(row)
+            res.append(row[0])
     f.close()
-    return res #return a list with one row per element
+    print(res)
+    # return a list with one row per element
+    return res
 
 
-#function creating a valid request URL from any title
 def correct_url(input_title):
+    '''create a valid request URL from any title'''
     output_title = ""
     for i in input_title:
         if i.isalnum() == True:
             output_title += i
         else:
             output_title += "+"
-    compatible_title = unidecode.unidecode(output_title)
-    res = urlAPI + compatible_title + keyAPI + country_query
+    res = urlAPI + output_title + keyAPI
     return res
 
 
-#function selecting relevant data in "volume" for the first 10 results
-#also prints time of the request
 def scraper(url):
+    '''select relevant data in "volume" for the first 10 results'''
+
+    # print time of the request
     print(f"Now scrapping URL: {url}")
     start_time = time()
-    response = requests.get(url) #API answer: <class 'requests.models.Response'>
+    response = requests.get(url) # API answer: <class 'requests.models.Response'>
     elapsed_time = time() - start_time
     print(f"Scrap intent done \n Request response: {response} \n Response delay : {round(elapsed_time, 2)} seconds")
-    page_content = response.text #turns page content into string
-    result_dct = json.loads(page_content) #create dictionnary from string content
-    n = result_dct["totalItems"] #number of results (object = int)
-    if n > 10:     # if number of results > 10, we set a limit
+
+    # turn page content into string
+    page_content = response.text
+
+    # create dictionnary from string content
+    result_dct = json.loads(page_content)
+
+    # number of results (object = int)
+    n = result_dct["totalItems"]
+
+    # if number of results > 10, we set a limit
+    if n > 10:
         n = 10
+
+    # elements containing info on all results (object = list)
     if "items" in result_dct:
-        all_volume_info = result_dct["items"] #elements containing info on all résults (object = list)
+        all_volume_info = result_dct["items"]
+
+    # create empty dict in a list if no search result
     else:
-        all_volume_info=[{"volumeInfo":""}] #creates empty dict in a list if no search result
-    res_dict = {} #dictionnary to gather n results of "items"
+        all_volume_info=[{"volumeInfo":""}]
 
+    # dictionnary to gather n results of "items"
+    res_dict = {}
+
+    # fetch info on the i^th book (object = dict)
     for i in range(0, n):
-        some_volume_info = all_volume_info[i] #fetching info on the ith book (object = dict)
-        volume_info = some_volume_info["volumeInfo"] #selecting the revelant info on this particular book (object = dict)
+        some_volume_info = all_volume_info[i]
+        # select the revelant info on this particular book (object = dict)
+        volume_info = some_volume_info["volumeInfo"]
 
-        res_list = [] #empty list to gather info on the book i, reinitialized if new book
-        msg_return_error = "N/A" #empty value if no data, to ensure that output CSV will have symetric columns
+        # empty list to gather info on the book i, reinitialized if new book
+        res_list = []
+        # empty value if no data, to ensure that output CSV will have symetric columns
+        msg_return_error = "N/A"
 
-        # defining the relevant metadata within input dict
+        # define the relevant metadata within input dict
         # all values are returned as a string if there's a corresponding key
         # apart from "authors", which is returned as a list (even if there's only one author)
         # if there is no corresponding key, the value returned will be the error message above
 
         if "title" in volume_info:
             volume_title = volume_info["title"]
+
         else:
             volume_title = msg_return_error
-        if "authors" in volume_info: # boucle qui produit un string d'un ou plusieurs auteurs
-            all_volume_authors = volume_info["authors"] #retourne une liste d'un ou plusieurs auteurs
+
+        # gather all author names of a specific volume
+        if "authors" in volume_info:
+            # list of one or several authors
+            all_volume_authors = volume_info["authors"]
+            # count the number of authors
             count = len(all_volume_authors)
+            # loop generating a string of one or several authors
             volume_authors = ""
             for j in range(0, count):
                 volume_authors += all_volume_authors[j]
-                if count>0 and j+1<count: #ajoute une virgule entre les noms si jamais
+                #adds a coma between names if necessary
+                if count>0 and j+1<count:
                     volume_authors += ", "
         else:
             volume_authors = msg_return_error
@@ -113,14 +144,14 @@ def scraper(url):
     return res_dict
 
 
-#buckle of all functions which returns a list from a CSV in input
-#also writes a CSV with all the result, a first row and the original book title
 def general_function(input_file):
-    with open("output_table.csv", "w", newline="", encoding="utf-8") as csvfile:
+    ''' loop of all functions which returns a list from a CSV in input
+     and write a CSV with all the result, a first row and the original book title'''
+    with open("scoring_todo.csv", "w", newline="", encoding="utf-8") as csvfile:
         final_res = []
         final_table = csv.writer(csvfile, delimiter="|", quotechar="'")
 
-        #creating first row
+        #create first row
         first_row = ["Original Title"]
         for n in range(1, 11):
             ntitle = [f"Result {n} - Title"]
@@ -133,7 +164,7 @@ def general_function(input_file):
             first_row.extend(ndate)
         final_table.writerow(first_row)
 
-        #creating list of original titles and URLs to be scrapped
+        #create list of original titles and URLs to be scrapped
         list_original_titles = []
         list_valid_URL = []
         for i in open_file(input_file):
@@ -158,6 +189,16 @@ def general_function(input_file):
             final_table.writerow(list_data)
             final_res.extend(list_data)
             print(f"Request {num_req} done\n New row added to the final table: {list_data} \n")
+            if num_req%100==0:
+                pause_time = time()
+                print("\nPause scraping (long)")
+                sleep(300)
+                print(f"Pause duration: {time() - pause_time}\n*5")
+            elif num_req%10==0:
+                pause_time = time()
+                print("\nPause scraping (short)")
+                sleep(10)
+                print(f"Pause duration: {time() - pause_time}\n")
         avg_time = total_time/num_req
         print(f"Average request time: {round(avg_time, 2)} seconds")
     return final_res
